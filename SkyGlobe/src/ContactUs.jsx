7dc93fb9d1f6887d77d41baed05520e7f86fddd5
dinithii-kdn <57031240+dinithii-kdn@users.noreply.github.com>
2021-09-01 21:52:09 +0530

    Update ContactUs.jsx

---
+++ SkyGlobe/src/ContactUs.jsx
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { Button } from 'react-bootstrap';
+
+
+import img1 from './images/img1.PNG';
+
+import Contact from './Contact';
+
+
+function App() {
+    return (
+        <div className="App">
+
+            <center><h1 style={{ fontFamily: 'monospace' }} size={100} >ABOUT US</h1></center>  <br />
+
+
+        </div >
