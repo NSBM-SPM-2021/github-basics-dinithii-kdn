96b11ed6c8bd982eb07ed54fd8ccad8c75495312
dinithii-kdn <57031240+dinithii-kdn@users.noreply.github.com>
2021-09-01 21:37:59 +0530

    First Commit

---
+++ SkyGlobe/src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { Button } from 'react-bootstrap';
+
+
+import img1 from './images/img1.PNG';
+
+import Contact from './Contact';
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <center><h1 style={{ fontFamily: 'monospace' }} size={100} >Welcome to FlightX !</h1></center>  <br />
+      <h3 style={{ color: 'purple' }}>Book Your Flight Ticket Here!</h3>
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+      <Button variant="primary" size="lg" >
+        GO TO NEXT PAGE
+      </Button>
+
+
+      <br /> <br /><br />
+      <img src={img1} alt="" />
+
+    </div >
+
+
+
+
+
+  );
+}
+
+export default App;
