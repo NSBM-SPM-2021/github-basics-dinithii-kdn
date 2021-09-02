559461e1dd75fb18394307263f2e3b389d480a24
dinithii-kdn <57031240+dinithii-kdn@users.noreply.github.com>
2021-09-02 13:33:45 +0530

    Update Home.jsx

--- SkyGlobe/src/Home.jsx
+++ SkyGlobe/src/Home.jsx
@@ -1,1 +1,20 @@
 import React from 'react'
+
+
+function App() {
+    return (
+        <div className="App">
+
+            <h1>WELCOME HOME PAGE</h1>
+
+        </div >
+
+
+
+
+
+    );
+}
+
+export default App;
+
