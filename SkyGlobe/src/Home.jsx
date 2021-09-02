b263274f89e20017d302d852f94f669d6038aabb
dinithii-kdn <57031240+dinithii-kdn@users.noreply.github.com>
2021-09-02 17:51:26 +0530

    Update Home.jsx

--- SkyGlobe/src/Home.jsx
+++ SkyGlobe/src/Home.jsx
@@ -27,6 +27,7 @@ function App() {
                                     Dropdown
                                 </a>
 
+
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                     <li><a class="dropdown-item" href="#">Action</a></li>
                                     <li><a class="dropdown-item" href="#">Another action</a></li>
