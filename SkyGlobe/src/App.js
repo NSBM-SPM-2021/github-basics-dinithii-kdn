1005f85588d170c830bd7c43893275e565368517
Dinithi Kindelpitiya <57031240+dinithii-kdn@users.noreply.github.com>
2021-10-11 21:58:49 +0530

    Update App.js

--- SkyGlobe/src/App.js
+++ SkyGlobe/src/App.js
@@ -1,44 +1,15 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Home from './Home';
 import './App.css';
 
-import { Button } from 'react-bootstrap';
-
-
-import img1 from './images/img1.PNG';
-
-import Contact from './Contact';
-import Home from './Home'
-import About from './About'
-import Contact from './Contact'
-
-
-
-
-function App() {
-  return (
-    <div className="App">
-
-
-      <h1>WELCOME HOME PAGE</h1>
-
-      <center><h1 style={{ fontFamily: 'monospace' }} size={100} >Welcome to FlightX !</h1></center>  <br />
-      <h3 style={{ color: 'purple' }}>Book Your Flight Ticket Here!</h3>
-      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
-      <Button variant="primary" size="lg" >
-        GO TO NEXT PAGE
-      </Button>
-
-
-      <br /> <br /><br />
-      <img src={img1} alt="" />
-
-    </div >
-
-
-
-
-
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Home />
+      </div>
+    );
+  }
 }
 
 export default App;
